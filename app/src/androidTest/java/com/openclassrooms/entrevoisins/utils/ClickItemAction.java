4bcd4f0d7624fdb4792ac7b705e031e310c454aa
Yohan <yohan@captiv.eu>
2019-08-26 00:39:27 +0200

    Added all instrumentation and unit test. All Passed.

---
+++ app/src/androidTest/java/com/openclassrooms/entrevoisins/utils/ClickItemAction.java
@@ -0,0 +1,28 @@
+package com.openclassrooms.entrevoisins.utils;
+
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.view.View;
+
+import com.openclassrooms.entrevoisins.R;
+
+import org.hamcrest.Matcher;
+
+public class ClickItemAction implements ViewAction {
+    @Override
+    public Matcher<View> getConstraints() {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Click on specific button";
+    }
+
+    @Override
+    public void perform(UiController uiController, View view) {
+        View button = view.findViewById(R.id.recycler_row);
+        // Maybe check for null
+        button.performClick();
+    }
+}
---
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/FavNeighbourFragment.java
@@ -0,0 +1,94 @@
+package com.openclassrooms.entrevoisins.ui.neighbour_list;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.openclassrooms.entrevoisins.R;
+import com.openclassrooms.entrevoisins.di.DI;
+import com.openclassrooms.entrevoisins.events.DeleteNeighbourEvent;
+import com.openclassrooms.entrevoisins.model.Neighbour;
+import com.openclassrooms.entrevoisins.service.DummyNeighbourGenerator;
+import com.openclassrooms.entrevoisins.service.NeighbourApiService;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
+import java.util.List;
+
+public class FavNeighbourFragment extends Fragment {
+
+    private NeighbourApiService mApiService;
+    static List<Neighbour> mNeighbours;
+    static RecyclerView mRecyclerView;
+
+    /**
+     * Create and return a new instance
+     * @return @{@link NeighbourFragment}
+     */
+    public static FavNeighbourFragment newInstance() {
+        FavNeighbourFragment fragment = new FavNeighbourFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mApiService = DI.getNeighbourApiService();
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_fav_neighbour, container, false);
+        Context context = view.getContext();
+        mRecyclerView = (RecyclerView) view;
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(context));
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
+        initList();
+
+        return view;
+    }
+
+    /**
+     * Init the List of neighbours
+     */
+    private void initList() {
+        mNeighbours = mApiService.getFav_neighbours();
+        mRecyclerView.setAdapter(new MyNeighbourRecyclerViewAdapter(mNeighbours));
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+
+    /**
+     * Fired if the user clicks on a delete button
+     * @param event
+     */
+    @Subscribe
+    public void onDeleteNeighbour(DeleteNeighbourEvent event) {
+        mApiService.deleteNeighbour(event.neighbour);
+        mApiService.deleteFavNeighbour(event.neighbour);
+        initList();
+    }
+}
